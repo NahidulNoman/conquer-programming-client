c975f3eb11f2dd4845cf4a02abec50ac06e9c61b
NahidulNoman <nahidulna5@gmail.com>
2022-10-26 00:17:41 +0600

    course details section added

--- src/components/CourseShareComponents/CourseDetails.jsx
+++ src/components/CourseShareComponents/CourseDetails.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Image } from "react-bootstrap";
 
 const CourseDetails = () => {
-    const details = useLoaderData();
-    console.log(details);
-    return (
-        <div>
-            <h2>yes we are waiting for it..</h2>
-        </div>
-    );
+  const detailsCourse = useLoaderData();
+  const { courseTittle, description, enrolled,courseName, img, topic } = detailsCourse;
+  return (
+    <div className="container mt-5 mb-5">
+      <Card className="">
+        <Card.Header>{courseName}</Card.Header>
+        <Card.Body>
+        <Image thumbnail className="w-50" src={img}></Image>
+          <Card.Title>{courseTittle}</Card.Title>
+          <Card.Text>
+            {description}
+            <h3>We Have Some Topic to cover this course : </h3>
+            {
+                topic.map(lesson => <li key={lesson}>{lesson}</li>)
+            }
+          </Card.Text>
+        </Card.Body>
+          <Button variant="primary" className="w-25 m-3">Get Premium Access</Button>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </div>
+  );
 };
 
 export default CourseDetails;
--- src/components/CourseShareComponents/ShowCartCourse.jsx
+++ src/components/CourseShareComponents/ShowCartCourse.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
 
 const ShowCartCourse = ({ cartCourse }) => {
   const { img, courseDuration, courseName, level,id, rating, price } = cartCourse;
@@ -14,10 +15,10 @@ const ShowCartCourse = ({ cartCourse }) => {
             <div className="card-body">
               <h5 className="card-title">{courseName}</h5>
               <p className="card-text">
-                <p className="m-0">Price : {price}</p>
+                <p className="m-0">Price : {price}$</p>
                 <p className="m-0">Level : {level}</p>
                 <p className="m-0">Course Duration : {courseDuration}</p>
-                <p className="m-0">Course Rating : {rating}</p>
+                <p className="m-0">Course Rating : <FaStar className="text-warning"></FaStar> {rating}</p>
               </p>
               <Link to={`/courseDetails/${id}`}><span className="btn btn-primary m-0">BUY COURSE</span></Link>
             </div>
