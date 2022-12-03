5ac9a668a6bd98a5fb991569cf90306375c29350
NahidulNoman <nahidulna5@gmail.com>
2022-12-03 21:19:43 +0600

    home section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import banner from '../asset/courses.png';
 
 const Home = () => {
   return (
-    <div className="container mt-5">
-      {" "}
-      <h2 className="text-primary fw-bold">Let's Conquer Programming...</h2>
-      <p className="text-dark fw-semibold opacity-75">
-        Computer programming is the process of performing a particular
-        computation (or more generally, <br /> accomplishing a specific
-        computing <br />
-        result), usually by designing and building an executable computer
-        program.
-      </p>
-      <Link to="/courses">
+    // <div className="container mt-5">
+    //   {" "}
+    //   <h2 className="text-primary fw-bold">Let's Conquer Programming...</h2>
+    //   <p className="text-dark fw-semibold opacity-75">
+    //     Computer programming is the process of performing a particular
+    //     computation (or more generally, <br /> accomplishing a specific
+    //     computing <br />
+    //     result), usually by designing and building an executable computer
+    //     program.
+    //   </p>
+      // <Link to="/courses">
+      //   {" "}
+      //   <button type="button" className="btn btn-outline-success fw-semibold">
+      //     Go To Courses <FaArrowRight></FaArrowRight>
+      //   </button>
+      // </Link>{" "}
+    // </div>
+    <Container>
+       <Row className="d-flex justify-content-center align-items-center">
+       <Col sm={12} lg={6}>
+            <div className="mt-5">
+            <h2 className="fw-bold fs-1">Learn without limits</h2>
+            <p className="fw-semibold">Start, switch, or advance your career with more than 5,400 courses, Professional Certificates, and degrees from world-class universities and companies.</p>
+            <Link to="/courses">
         {" "}
         <button type="button" className="btn btn-outline-success fw-semibold">
           Go To Courses <FaArrowRight></FaArrowRight>
         </button>
       </Link>{" "}
-    </div>
+            </div>
+        </Col>
+        <Col sm={12} lg={6}>
+          <img src={banner} className='image-fluid w-75 me-5' alt="" />
+        </Col>
+       </Row>
+    </Container>
   );
 };
 
