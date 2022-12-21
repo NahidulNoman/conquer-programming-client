331f903a9fde7f1cf28b6c558ec90b8f822e2a7e
NahidulNoman <nahidulna5@gmail.com>
2022-12-21 21:33:23 +0600

    main section modify

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer/Footer';
-import NavBar from '../components/NavBar/NavBar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
 
 const Main = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
